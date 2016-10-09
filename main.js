9c712204d067aaf7b0c0683c1234fdf321409b53
Adam Sidiali <adamsidiali@gmail.com>
2016-10-09 14:34:56 -0700

    wip

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-$('.mobile-nav-toggle').click(function () {
-  toggleMenu();
+$(function () {
+    console.log('ready');
+    $('#fullpage').fullpage({
+      anchors:['home', 'mission']
+    });
+
+    $('.mobile-nav-toggle').click(function () {
+      console.log('clicked');
+      toggleMenu();
+    });
+
+
+    $('div.navbar-bg').click(function () {
+      toggleMenu();
+    });
+
+    $('ul.navbar li').click(function () {
+      toggleMenu();
+    });
+
+
+    function toggleMenu() {
+      $('ul.navbar').toggleClass('open');
+      $('div.navbar-bg').toggleClass('open');
+
+      var txt = $('.mobile-nav-toggle i').text();
+      $('.mobile-nav-toggle i').text((txt === 'menu') ? 'close' : 'menu');
+    }
+
 });
-
-$('div.navbar-bg').click(function () {
-  toggleMenu();
-});
-
-function toggleMenu() {
-  $('ul.navbar').toggleClass('open');
-  $('div.navbar-bg').toggleClass('open');
-
-  var txt = $('.mobile-nav-toggle i').text();
-  $('.mobile-nav-toggle i').text((txt === 'menu') ? 'close' : 'menu');
-}
