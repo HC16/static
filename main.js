31be36a46fad9bde12d435a9d21dedafe6771b0d
Adam Sidiali <adamsidiali@gmail.com>
2016-10-09 11:59:45 -0700

    wip

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 $('.mobile-nav-toggle').click(function () {
+  toggleMenu();
+});
 
+$('.div.navbar-bg.open').click(function () {
+  toggleMenu();
+});
+
+function toggleMenu() {
   $('ul.navbar').toggleClass('open');
   $('div.navbar-bg').toggleClass('open');
 
   var txt = $('.mobile-nav-toggle i').text();
   $('.mobile-nav-toggle i').text((txt === 'menu') ? 'close' : 'menu');
-  console.log(txt);
-});
+}
